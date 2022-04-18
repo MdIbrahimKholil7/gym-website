0a0d80983d727627979a5f7ce75b5aa67c73c360
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-04-18 23:47:39 +0600

    img change

--- src/hooks/useService.js
+++ src/hooks/useService.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react'
 import service1 from '../images/service4.jpg'
-import service2 from '../images/service2.jpg'
+import service2 from '../images/service10.jpg'
 import service3 from '../images/service3.jpg'
+import service4 from '../images/service6.jpg'
+import service5 from '../images/service7.jpg'
+import service6 from '../images/service8.jpg'
+import service7 from '../images/service9.jpg'
 const details = [
     {
         img: service1,
@@ -11,21 +15,21 @@ const details = [
         price:13
     },
     {
-        img: service2,
+        img: service7,
         name: 'Personal Training',
         desc: 'A personal fitness trainer will work with your group of 2-4 people to help you achieve your fitness goals.',
         id: 11,
         price:11
     },
     {
-        img: service3,
+        img: service4,
         name: 'Customized Group Training',
         desc: 'Find your group, find your trainer, book your time! Three easy steps to set up your own small group training sessions with a certified City of Edmonton Personal Trainer.',
         id: 12,
         price:7
     },
     {
-        img: service1,
+        img: service5,
         name: 'Private Personal Training',
         desc: 'A personal fitness trainer will work with you one-on-one to create a custom program based on your goals, history and needs. ',
         id: 13,
@@ -33,14 +37,14 @@ const details = [
 
     },
     {
-        img: service3,
+        img: service6,
         name: 'Customized Group Training',
         desc: 'Find your group, find your trainer, book your time! Three easy steps to set up your own small group training sessions with a certified City of Edmonton Personal Trainer.',
         id: 14,
         price:15
     },
     {
-        img: service1,
+        img: service2,
         name: 'Private Personal Training',
         desc: 'A personal fitness trainer will work with you one-on-one to create a custom program based on your goals, history and needs. ',
         id: 15,
