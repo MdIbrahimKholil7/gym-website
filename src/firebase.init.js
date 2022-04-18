f3104f2bba01ba73107603ea9c0e37389eadaa27
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-04-18 23:18:17 +0600

    api key added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,7 +5,7 @@ import { getAuth } from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey:process.env.REACT_APP_apiKey,
   authDomain:process.env.REACT_APP_authDomain,
   projectId:process.env.REACT_APP_projectId,
@@ -13,15 +13,15 @@ import { getAuth } from 'firebase/auth'
   messagingSenderId:process.env.REACT_APP_messagingSenderId,
   appId:process.env.REACT_APP_appId
 
-}; */
-const firebaseConfig = {
+};
+/* const firebaseConfig = {
   apiKey: "AIzaSyBgC_oz0ADfkRsrjiLdnchMtiIBhi9IUIk",
   authDomain: "gym-project-2.firebaseapp.com",
   projectId: "gym-project-2",
   storageBucket: "gym-project-2.appspot.com",
   messagingSenderId: "149775812735",
   appId: "1:149775812735:web:779c7be419cf13d057974a"
-};
+}; */
 console.log(process.env.REACT_APP_apiKey)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
